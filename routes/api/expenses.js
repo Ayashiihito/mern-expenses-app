7cc77b75d39754fb79567c86f7856b36f5d0896a
Vladimir <antsupov.vova@gmail.com>
2019-04-03 21:02:32 +0300

    delete route

--- routes/api/expenses.js
+++ routes/api/expenses.js
@@ -3,15 +3,27 @@ const router = express.Router();
 
 const Expense = require('../../models/Expense');
 
+// get a list of all expenses
+// PUBLIC
 router.get('/', (req, res) => {
   Expense.find().then(expenses => res.json(expenses));
 });
 
+// put an expense in DB
+//PUBLIC
 router.post('/', (req, res) => {
   const expense = new Expense({
     value: req.body.value,
     expType: req.body.expType,
   });
   expense.save().then(expense => res.json(expense));
+});
+
+//delete an expense from DB
+//PUBLIC
+router.delete('/:id', (req, res) => {
+  Expense.findById(req.params.id)
+    .then(expense => expense.remove().then(() => res.json({ success: true })))
+    .catch(err => res.status(404).json({ success: false }));
 });
 module.exports = router;
