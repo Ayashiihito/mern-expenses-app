22f8f975080de02b561ed410b0d9e04ab264a45e
Vladimir <antsupov.vova@gmail.com>
2019-04-12 16:33:13 +0300

    fix timeout error

--- server.js
+++ server.js
@@ -33,20 +33,17 @@ if (process.env.NODE_ENV !== 'production') {
 require('./config/passport')(passport);
 
 //Routes:
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
-  app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
-}
 
 app.use('/api/expenses', expenses);
 app.use('/api/users', users);
 app.use('/api/expTypes', expTypes);
 
-app.get('*', (req, res) => {
-  res.status(404);
-});
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 app.listen(process.env.PORT || PORT, () =>
   console.log(`Started server at ${PORT}`)
